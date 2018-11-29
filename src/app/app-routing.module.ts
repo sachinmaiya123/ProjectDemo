cfd9c96cfccf5a81de518d08e1f3932cae94102c
Sachin Maiya <maiyasachin13@gmail.com>
2018-11-29 08:35:36 +0530

    jhjhj
    hgygy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './login/home/home.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+    {
+      path: '',
+      component: LoginComponent
+    },
+    {
+    path: 'home',
+    component: HomeComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
